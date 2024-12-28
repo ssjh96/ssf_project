0483816abf00cb02969013098fd6c731f82c0007
ssjh96 <ssjh96@outlook.com>
2024-12-28 17:11:43 +0800

    Refined UI slightly, Shifted API Data Access Point to Home page

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ssf_project
+1. Change /delete to host & admin can delete all events
+2. Display a random meal suggestion
+3. Check if httpresponse is required
+4. Change requestparam to pathvariables
+5. Change invitation status
--- src/main/java/sg/edu/nus/iss/ssf_project_potluck_shamus/controller/EventController.java
+++ src/main/java/sg/edu/nus/iss/ssf_project_potluck_shamus/controller/EventController.java
@@ -280,18 +280,9 @@ public class EventController
     }
     
     @GetMapping("/viewapi")
-    public String viewApiData(@RequestParam("eventId") String eventId, 
-                            Model model) throws ParseException 
-    {
-        EventModel event = eventService.getEvent(eventId);
-
-        
-        // List<String> letters = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
-        //                                                             "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"));
-
-        model.addAttribute("event", event);
+    public String viewApiData(Model model) throws ParseException 
+    {       
         model.addAttribute("categories", categoryService.fetchCategories());
-        // model.addAttribute("letters", letters);
 
         return "viewapi";
     }
